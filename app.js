ff0c18a0e7363d9bbf5ccbe4b048e50526899bfd
Impr3ssio <Impr3ssio@gmail.com>
2025-03-16 14:23:55 +0530

    removed mongo client

--- app.js
+++ app.js
@@ -443,31 +443,6 @@ app.post('/cart/delete/:index', ensureAuthenticated, async (req, res) => {
     res.redirect('/cart');
   }
 });
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://Shubh:ShubhP@ndey123@impressio.k358d.mongodb.net/?retryWrites=true&w=majority&appName=impressio";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
